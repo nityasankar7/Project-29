ec285de5c6ec227bc2abfa8c127751231f05b497
nityasankar7 <70414577+nityasankar7@users.noreply.github.com>
2020-10-24 12:04:21 -0400

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -3,7 +3,7 @@ class SlingShot{
        var options = {
             bodyA: bodyA,
             pointB: pointB,
-            stiffness: 1.1,
+            stiffness: 1,
             length: 500
         }
         this.pointB = pointB
@@ -13,7 +13,7 @@ class SlingShot{
     
     }
     fly(){
-    this.sling.bodyA=null;
+    this.sling1.bodyA=null;
     }
     display(){
     
